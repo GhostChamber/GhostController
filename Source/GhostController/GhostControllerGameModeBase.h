a5b0c50413fc1a9efbb660e6a09cb9445c734675
Sanjay Nambiar <sanjay.nambiar@ucf.edu>
2017-04-26 12:55:47 -0400

    Added basic UE4 android app code

---
+++ Source/GhostController/GhostControllerGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "GhostControllerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOSTCONTROLLER_API AGhostControllerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
